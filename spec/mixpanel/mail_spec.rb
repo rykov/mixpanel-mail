fb574d3b54b7df5df7a9de7f4e52ae00380ab8cb
Michael Rykov <mrykov@gmail.com>
2011-09-26 18:52:40 -0700

    Add Webmock specs to test HTTP requests

--- spec/mixpanel/mail_spec.rb
+++ spec/mixpanel/mail_spec.rb
@@ -23,14 +23,14 @@ describe Mixpanel::Mail do
         'bad_bad' => nil
       })
     end
-    
+
     it 'should recognize "click_tracking" option' do
-      mp_option_check('click_tracking', { 
+      mp_option_check('click_tracking', {
         true => true,
         false => '0'
       })
     end
-    
+
     it 'should recognize "properties" option' do
       example = { :hello => :world }
       mp_option_check('properties', {
@@ -39,7 +39,7 @@ describe Mixpanel::Mail do
         example => MultiJson.encode(example)
       })
     end
-    
+
     it 'should recognize "redirect_host" option' do
       mp_option_check('redirect_host', {
         nil => nil, 'anything' => 'anything'
@@ -47,15 +47,62 @@ describe Mixpanel::Mail do
     end
   end
 
+  describe 'add tracking' do
+    it 'should make a simple request' do
+      verify_request_with_options
+    end
+
+    it 'should make a request with "type" request' do
+      verify_request_with_options(
+        { :type  => 'text' },
+        { 'type' => 'text' }
+      )
+    end
+
+    it 'should make a request with "click_tracking" request' do
+      verify_request_with_options(
+        { :click_tracking  => false },
+        { 'click_tracking' => '0' }
+      )
+    end
+
+    it 'should make a request with "properties" request' do
+      props = { :foo => 'bar' }
+      verify_request_with_options(
+        { :properties  => props },
+        { 'properties' => MultiJson.encode(props) }
+      )
+    end
+
+    it 'should make a request with "redirect_host" request' do
+      verify_request_with_options(
+        { :redirect_host  => 'http://www.google.com' },
+        { 'redirect_host' => 'http://www.google.com' }
+      )
+    end
+  end
+
 private
   def mp_mail(options = {})
     ::Mixpanel::Mail.new(TOKEN, CAMPAIGN, options)
   end
-  
+
   def mp_option_check(key, value_expectations = {})
     value_expectations.each do |value, expectation|
       mp_mail(key => value).params[key].should eq(expectation)
       mp_mail(key.to_sym => value).params[key].should eq(expectation)
     end
+  end
+
+  def verify_request_with_options(options = {}, expectations = {})
+    stub_post
+    mp_mail(options).add_tracking('my-dist-id', 'Hello World!')
+    mp_mail.add_tracking('my-dist-id', 'Hello World!', options)
+    a_post.with(:body => expectations.merge(
+      'token' => TOKEN,
+      'campaign' => CAMPAIGN,
+      'distinct_id' => 'my-dist-id',
+      'body' => 'Hello World!'
+    )).should have_been_made.twice
   end
 end
--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,10 +1,10 @@
 # Define a_get, a_post, etc and stub_get, stub_post, etc
 [:delete, :get, :post, :put].each do |method|
-  self.class.send(:define_method, "a_#{method}") do |path|
-    a_request(method, Gemfury.endpoint + path)
+  self.class.send(:define_method, "a_#{method}") do
+    a_request(method, Mixpanel::Mail::ENDPOINT)
   end
 
-  self.class.send(:define_method, "stub_#{method}") do |path|
-    stub_request(method, Gemfury.endpoint + path)
+  self.class.send(:define_method, "stub_#{method}") do
+    stub_request(method, Mixpanel::Mail::ENDPOINT)
   end
 end
